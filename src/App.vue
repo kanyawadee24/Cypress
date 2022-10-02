15ca260872494842de91ba28fe59796f7ee1a9a3
Wongsakorn Kongosod <wongsakorn.ku@gmail.com>
2022-10-02 23:27:39 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -29,8 +29,8 @@ export default {
   data() {
     return {
       ToDoItems: [
-        { id: uniqueId('todo-'), label: 'Learn Vue', done: false },
-        { id: uniqueId('todo-'), label: 'Create a Vue project with the CLI', done: true },
+        { id: uniqueId('todo-'), label: 'Learn Cypress', done: false },
+        { id: uniqueId('todo-'), label: 'Create a Cypress project', done: true },
         { id: uniqueId('todo-'), label: 'Have fun', done: true },
         { id: uniqueId('todo-'), label: 'Create a to-do list', done: false }
       ]
